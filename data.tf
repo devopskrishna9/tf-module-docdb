48388b2550f93b5951d2fd23303a6514e3e22790
krishna <brkkrishna9@gmail.com>
2023-02-20 21:01:20 +0530

    new changes

--- data.tf
+++ data.tf
@@ -1,8 +1,8 @@
 data "aws_ssm_parameter" "DB_ADMIN_USER" {
-  name = "DB_ADMIN_USER"
+  name = "${var.env}.dev.docdb.DB_ADMIN_USER"
 }
 
 data "aws_ssm_parameter" "DB_ADMIN_PASS" {
-  name = "DB_ADMIN_PASS"
+  name = "${var.env}.dev.docdb.DB_ADMIN_PASS"
 }
 
