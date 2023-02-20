ae004c286aab120e190f5c1344686261bdb1b684
krishna <brkkrishna9@gmail.com>
2023-02-20 20:18:58 +0530

    new changes

--- main.tf
+++ main.tf
@@ -33,3 +33,17 @@ resource "aws_security_group" "docdb" {
     { Name = "${var.env}-docdb-surity_group" }
   )
 }
+
+resource "aws_docdb_cluster" "docdb" {
+  cluster_identifier      = "${var.env}-docdb-cluster"
+  engine                  = "docdb"
+  master_username         = "foo"
+  master_password         = "mustbeeightchars"
+  skip_final_snapshot     = true
+  db_subnet_group_name = aws_docdb_subnet_group.default.name
+  vpc_security_group_ids = [aws_security_group.docdb.id]
+  tags = merge(
+    local.common_tags,
+    { Name = "${var.env}-docdb-cluster" }
+  )
+}
