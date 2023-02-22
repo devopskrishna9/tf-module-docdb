036317c094f79df1f33471d8a4446e1430f9cb2f
krishna <brkkrishna9@gmail.com>
2023-02-22 14:37:35 +0530

    new changes

--- data.tf
+++ data.tf
@@ -6,6 +6,6 @@ data "aws_ssm_parameter" "DB_ADMIN_PASS" {
   name = "${var.env}.docdb.DB_ADMIN_PASS"
 }
 
-data "aws_kms_alias" "key" {
-  key_id = "roboshop"
+data "aws_kms_key" "by_id" {
+  key_id = "d3bd81d9-9679-44f3-8d22-98fbf65735dc"
 }
