4b0999a9742dc0d4d0eafb73d149efff8a1c98ce
krishna <brkkrishna9@gmail.com>
2023-02-22 14:40:52 +0530

    new changes

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ resource "aws_docdb_cluster" "docdb" {
   db_subnet_group_name = aws_docdb_subnet_group.default.name
   vpc_security_group_ids = [aws_security_group.docdb.id]
   storage_encrypted = true
-  kms_key_id = data.aws_kms_alias.key.arn
+  kms_key_id = data.aws_kms_key.by_id.arn
 
   tags = merge(
     local.common_tags,
@@ -58,7 +58,7 @@ resource "aws_docdb_cluster_instance" "cluster_instances" {
   cluster_identifier = aws_docdb_cluster.docdb.id
   instance_class     = var.instance_class
   storage_encrypted = true
-  kms_key_id = data.aws_kms_alias.key.arn
+  kms_key_id = data.aws_kms_key.by_id.arn
 
   tags = merge(
     local.common_tags,
